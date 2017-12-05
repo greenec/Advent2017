a20f76316ab3bdbfe40a30a8a27d402aa215bea1
Connor Greene <connor.greene2000@gmail.com>
2017-12-05 11:51:44 -0500

    Completed Day 4

---
+++ 4/4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                String line;
+                int numValid = 0;
+
+                System.IO.StreamReader file = new System.IO.StreamReader("../../Input.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (isValidPassphrase2(line))
+                    {
+                        numValid++;
+                    }
+                }
+                file.Close();
+
+                Console.WriteLine(numValid);
+                Console.ReadLine();
+            }
+        }
+
+        static bool isValidPassphrase(String input)
+        {
+            String[] arr = input.Split(' ');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && arr[i] == arr[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static bool isValidPassphrase2(String input)
+        {
+            String[] arr = input.Split(' ');
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = String.Concat(arr[i].OrderBy(c => c));
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && arr[i] == arr[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
