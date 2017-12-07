63c7d0ff8b80fa83a91f9c7283edff960f05e6f2
Connor Greene <connor.greene2000@gmail.com>
2017-12-07 11:51:30 -0500

    Started day 6

---
+++ 6/6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> banks = new List<int>();
+            List<List<int>> history = new List<List<int>>();
+
+            history.Add(banks);
+
+            
+        }
+
+        static 
+    }
+}
