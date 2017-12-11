6744d2bb0f13eb6bf27dd6b475f20d1e47d8ad8a
Connor Greene <connor.greene2000@gmail.com>
2017-12-11 08:35:58 -0500

    Completed day 9

---
+++ 9/9/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String stream = File.ReadAllText("../../Input.txt");
+            bool inGarbage = false;
+            int parentScore = 0, score = 0, garbageCount = 0;
+
+            for (int i = 0; i < stream.Length; i++)
+            {
+                if (stream[i] == '!')
+                {
+                    i++;
+                    continue;
+                }
+
+                if (inGarbage)
+                {
+                    if(stream[i] == '>')
+                    {
+                        inGarbage = false;
+                        continue;
+                    }
+                    garbageCount++;
+                    continue;
+                }
+
+                if (stream[i] == '<')
+                {
+                    inGarbage = true;
+                    continue;
+                }
+
+                if (stream[i] == '{')
+                {
+                    parentScore++;
+                    score += parentScore;
+                    continue;
+                }
+
+                if(stream[i] == '}')
+                {
+                    parentScore--;
+                    continue;
+                }
+            }
+            
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Garbage count: " + garbageCount);
+            Console.ReadLine();
+        }
+    }
+}
