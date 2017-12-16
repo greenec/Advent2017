3ce1fe2148ef02698a229bc4b997a193b7a15066
Connor Greene <connor.greene2000@gmail.com>
2017-12-16 11:02:19 -0500

    Completed day 15

---
+++ 15/15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger a = 277, b = 349, count = 0;
+
+            for (int i = 0; i < 5000000; i++)
+            {
+                a = (a * 16807) % 2147483647;
+                while (a % 4 != 0)
+                {
+                    a = (a * 16807) % 2147483647;
+                }
+
+                b = (b * 48271) % 2147483647;
+                while (b % 8 != 0)
+                {
+                    b = (b * 48271) % 2147483647;
+                }
+
+                if (a % 0x10000 == b % 0x10000)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
