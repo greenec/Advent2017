e6a41f76458c1c028ab842991e08fbef9118811a
Connor Greene <connor.greene2000@gmail.com>
2017-12-17 19:42:46 -0500

    Completed day 17

---
+++ Day 17/Day 17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 0, steps = 355;
+            List<int> list = new List<int> { 0 };
+
+            for (int i = 1; i < 2018; i++)
+            {
+                pos = (pos + steps) % list.Count + 1;
+                list.Insert(pos, i);
+            }
+
+            int output = 0;
+
+            for (int i = 1; i < 50000000; i++)
+            {
+                pos = (pos + steps) % i + 1;
+                if (pos == 1)
+                {
+                    output = i;
+                }
+            }
+
+            //Console.WriteLine(list[pos + 1]);
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
