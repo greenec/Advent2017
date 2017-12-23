e44e1146cc4511639f5cf63f46a07f861432a8f1
Connor Greene <connor.greene2000@gmail.com>
2017-12-23 16:24:20 -0500

    Began day 12

---
+++ 12/12/Entry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Entry
+    {
+        public int number;
+        public List<int> children;
+
+        public Entry(int n, List<int> c)
+        {
+            number = n;
+            children = c;
+        }
+    }
+}
---
+++ 12/12/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] input = File.ReadAllLines("../../Input.txt");
+
+            bool flag = true;
+            int count = 1;
+            List<Entry> programs = new List<Entry>();
+            List<int> touchesZero = new List<int>() { 0 };
+
+            foreach (String line in input)
+            {
+                int programNum = int.Parse(line.Substring(0, line.IndexOf(" ")));
+                List<int> children = line.Substring(line.IndexOf(">") + 1).Replace(" ", "").Split(',').Select(int.Parse).ToList();
+                programs.Add(new Entry(programNum, children));
+            }
+
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < programs.Count; i++)
+                {
+                    if (programs[i].number == 0)
+                    {
+                        touchesZero.AddRange(programs[i].children);
+                        count += programs[i].children.Count;
+                        programs.RemoveAt(i);
+                        Console.WriteLine("Adding: " + String.Join(", ", programs[i].children));
+                        i--;
+                        continue;
+                    }
+
+                    if (programs[i].children.Intersect(touchesZero).ToList().Count != 0)
+                    {
+                        flag = true;
+                        programs.RemoveAt(i);
+                        Console.WriteLine("Adding: " + String.Join(", ", programs[i].children));
+                        count++;
+                        i--;
+                    }
+                }
+                if (!flag)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
